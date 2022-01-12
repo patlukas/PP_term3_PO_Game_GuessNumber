79f55c9e88f6189dbc79bb6d807d372eb881c267
48724 <lukaszewski.patryk06@gmail.com>
2022-01-12 01:10:50 +0100

    Gotowy podstawowy szkielet dzialania. Odróżnia odgadnięty wynik od za małego/dużego. Działa stoper liczący pełne sekundy, działa licznik prób.

--- src/com/company/GuessNumberPage.java
+++ src/com/company/GuessNumberPage.java
@@ -1,22 +1,27 @@
 package com.company;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.util.Comparator;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuessNumberPage extends JFrame{
     private JTable table1;
-    private JTextField textField1;
+    private JTextField guessNumberTextField;
     private JButton startButton;
-    private JButton zgadujButton;
+    private JButton guessButton;
     private JPanel mainPanel;
+    private JLabel numberOfGuessLabel;
+    private JLabel numberTipLabel;
+    private JLabel timerLabel;
+    private Integer numberOfGuess, solutionNumber, numberOfSeconds;
 
     public GuessNumberPage() {
         setupFrame();
 
         startButton.addActionListener(e -> startNewGame());
+        guessButton.addActionListener(e -> guessNumber());
+        guessNumberTextField.addActionListener(e -> guessNumber());
     }
 
     private void setupFrame() {
@@ -24,9 +29,52 @@ public class GuessNumberPage extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(500, 400));
         setVisible(true);
+        setVisibleTheGuessingWidgets(false);
     }
 
     private void startNewGame() {
-        return;
+        timer.stop();
+        numberOfGuess = 0;
+        solutionNumber = 55;
+        numberOfSeconds = 0;
+        timer.start();
+        numberTipLabel.setText("");
+        numberOfGuessLabel.setText("Numer próby: 0");
+        setVisibleTheGuessingWidgets(true);
+        guessNumberTextField.grabFocus();
+        System.out.println("Start");
     }
+
+    private void guessNumber() {
+        numberOfGuess++;
+        numberOfGuessLabel.setText("Numer próby: "+numberOfGuess);
+        String numberString = guessNumberTextField.getText();
+        Integer number = Integer.parseInt(numberString);
+        guessNumberTextField.setText("");
+        guessNumberTextField.grabFocus();
+        if(number < solutionNumber) {
+            numberTipLabel.setText("Liczba "+number+" jest MNIEJSZA od rozwiązania");
+        } else if(number > solutionNumber) {
+            numberTipLabel.setText("Liczba "+number+" jest WIĘKSZA od rozwiązania");
+        } else {
+            numberTipLabel.setText("SUKCES!!! Szukana liczba to "+number+"!!!");
+            setVisibleTheGuessingWidgets(false);
+            timer.stop();
+        }
+
+        System.out.println("Zgaduj");
+    }
+
+    private void setVisibleTheGuessingWidgets(Boolean visible) {
+        guessNumberTextField.setVisible(visible);
+        guessButton.setVisible(visible);
+    }
+
+    Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            numberOfSeconds++;
+            timerLabel.setText("Czas: "+numberOfSeconds+"s");
+        }
+    });
 }
